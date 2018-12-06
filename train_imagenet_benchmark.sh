544800f98d7c0bb1daf86bcff5eb16e2e8c55201
cx2 <cx2@illinois.edu>
2018-12-06 14:59:45 -0600

    add benchmark

--- train_imagenet_benchmark.sh
+++ train_imagenet_benchmark.sh
@@ -6,5 +6,5 @@ source activate mxnet
 export OMP_NUM_THREADS=88;
 export KMP_AFFINITY=granularity=fine,compact,1,0;
 
-cd ~/federated
+cd ~/federated/robust_fedavg
 time numactl -p 1 python train_imagenet_benchmark.py 2>&1 | tee ~/federated/results/train_imagenet_benchmark.log

7eb518771ba226031252a5f4298ee74bd1544764
cx2 <cx2@illinois.edu>
2018-12-06 14:57:20 -0600

    add benchmark

---
+++ train_imagenet_benchmark.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=272 -lplace=excl
+
+source activate mxnet
+
+export OMP_NUM_THREADS=88;
+export KMP_AFFINITY=granularity=fine,compact,1,0;
+
+cd ~/federated
+time numactl -p 1 python train_imagenet_benchmark.py 2>&1 | tee ~/federated/results/train_imagenet_benchmark.log

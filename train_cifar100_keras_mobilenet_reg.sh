eac8aadc98b93755ba0f5f669a38d52c7cc90ea5
cx2 <cx2@illinois.edu>
2018-12-13 03:57:53 -0600

    add regularizer

---
+++ train_cifar100_keras_mobilenet_reg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=272 -lplace=excl
+
+source activate tensorflow_mpi
+
+export OMP_NUM_THREADS=136;
+export KMP_AFFINITY=granularity=fine,compact,1,0;
+
+cd ~/federated/robust_fedavg
+time numactl -p 1 python train_cifar100_keras_mobilenet_reg.py --nsplit 40 --batchsize 256 --epochs 800 --dir /homes/cx2/datasets/cifar100 2>&1 | tee ~/federated/results/train_cifar100_keras_mobilenet_reg.log

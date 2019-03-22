f7964dea16194439a3e5d4ad8450c05092c8ffba
cx2 <cx2@illinois.edu>
2019-03-22 16:47:48 -0500

    bptt

--- experiment_script_4.sh
+++ experiment_script_4.sh
@@ -10,7 +10,7 @@ export I_MPI_FABRICS=ofi
 export I_MPI_TMI_PROVIDER=psm2
 export HFI_NO_CPUAFFINITY=1
 export I_MPI_FALLBACK=0
-export OMP_NUM_THREADS=28
+export OMP_NUM_THREADS=14
 
 ### OPA FABRIC ###
 export I_MPI_HYDRA_BOOTSTRAP=rsh
@@ -33,4 +33,4 @@ logfile=/homes/cx2/federated/results/fedrob_4.txt
 > $logfile
 
 cat $PBS_NODEFILE | uniq > $PBS_O_WORKDIR/hostfile
-mpirun -np 10 -machinefile $PBS_O_WORKDIR/hostfile python /homes/cx2/federated/fedrob/fedrob_bptt.py --model default --nsplit 100 --nbyz 0 --trim 0 --lr 20 --alpha 1 --alpha-decay 0.9 --alpha-decay-epoch 400 --epochs 800 --iterations 2 --seed 733 --dir $inputdir --valdir $valdir --log $logfile 2>&1 | tee $watchfile
+mpirun -np 10 -machinefile $PBS_O_WORKDIR/hostfile python /homes/cx2/federated/fedrob/fedrob_bptt.py --model default --nsplit 100 --nbyz 0 --trim 0 --lr 20 --alpha 1 --alpha-decay 0.9 --alpha-decay-epoch 300 --epochs 400 --iterations 2 --seed 733 --dir $inputdir --valdir $valdir --log $logfile 2>&1 | tee $watchfile
